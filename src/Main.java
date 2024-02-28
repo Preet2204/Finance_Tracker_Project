806136863927fae78d769027f2b065f6ccb0ae3b
Akshat1815 <akshats1815@gmail.com>
2024-02-28 15:06:36 +0530

    back to og

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 import java.util.Scanner;
 
-private class User{
-    private String pass=new String();
-    check 
-}
-
-
-public class Main {
-    User one=new User();
+public class Main {   
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("enter your password");
-        String pass=s.nextLine();
-        boolean isUser=one.checkPass(pass);
     }
 }
