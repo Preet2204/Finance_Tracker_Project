0db5a4ad1dc1c5e420239b34b0886c24171bb82c
Akshat1815 <akshats1815@gmail.com>
2024-02-28 14:46:27 +0530

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+private class User{
+    private String pass=new String();
+    check 
+}
+
+
 public class Main {
+    User one=new User();
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter your password");
+        String pass=s.nextLine();
+        boolean isUser=one.checkPass(pass);
     }
 }
