24f6ff13e8982931e94a46087d624c316cc031d4
Akshat1815 <157917312+Akshat1815@users.noreply.github.com>
2024-04-05 17:34:44 +0530

    Update Main.java
    Signin & Signup

--- src/Main.java
+++ src/Main.java
@@ -17,37 +17,39 @@ import java.util.Scanner;
 // }
 
 class User{
-
-    protected String pass=new String();
-    protected String userName = new String();
-
-    void makeChoice1(int ch)
+    Scanner sca = new Scanner(System.in);
+    private String pass=new String();
+    private String userName = new String();
+    
+    void makeChoice(int ch)
     {
-        switch (ch) {
-            case 1:
-                
-                break;
         
-            default:
-                break;
-        }
     }
 
-    User(String usernaam) {
-        userName=usernaam;
+    void setUserName(String username){
+        this.userName=username;
     }
-
-    String getUserName() {
+    String getUserName()
+    {
         return userName;
+    }
+    void setPass(String password){
+        this.pass=password;
+    }
+    String getPass()
+    {
+        return pass;
     }
 
     void makeUser(){
-        Scanner sca = new Scanner(System.in);
+        System.out.println("Enter User Name: ");
+        setUserName(sca.nextLine());
+        System.out.println("Create Password: ");
         String password = sca.nextLine();
         boolean hasUpper=false;
         boolean hasLower=false;
         boolean hasDigit=false;
-        if(password.length()>=5)                            //password Strength Checker (Length >= 5, Atleast one uppercase and lowercase)
+        if(password.length()>=5)
         {
             for(int i=0;i<password.length();i++)
             {
@@ -64,7 +66,7 @@ class User{
                 }
             }
             if(hasUpper && hasDigit && hasLower)
-            pass=password;
+            setPass(password);
             else{
                 System.out.println("Invalid Password");
                 System.out.println("Password Must have atleast 1 UpperCase, atleast 1 Lower Case and atleast 1 Digit.");
@@ -81,64 +83,73 @@ class User{
     }
 }
 
-class Account extends User{
-    Scanner sca = new Scanner(System.in);
-
-    double balance;
-    float interestRate;
-
-    Account(String usernaam) {
-        super(usernaam);
-    }
-
-    void makeAccount() {
-        System.out.println("The following Accounts are available for your use: ");
-        System.out.println("1. Savings Account");
-        System.out.println("2. Checkings Account");
-        System.out.println("Enter the appropriate Digit to proceed further");
-        int ch = sca.nextInt();
-        switch (ch) {
-            case 1:
-                
-                break;
-            case 2:
-
-                break;
-            default:
-                break;
-        }
-    }
-
-    
-}
-
-class Savings extends Account{
-
-    Savings(String usernaam) {
-        super(usernaam);
-    }
-    
-}
 
 public class Main {   
-    static User one;
+    static ArrayList<User> users=new ArrayList<>();
     static Scanner sca = new Scanner(System.in);
+    static int ind = 0;
+    static void signup(ArrayList<User> use) {
+        boolean userExists=false;
+        for(int i=0;i<use.size();i++)
+        {
+            System.out.println("Enter User Name: ");
+            String username=sca.nextLine();
+            if(use.get(i).getUserName()==username)
+            {
+                userExists=true;
+                break;
+            }
+        }
+        if(userExists){
+            System.out.println("UserName already exists, enter a new username!");
+            signup(use);
+        }
+        use.add(new User());
+        use.get(ind++).makeUser();
+    }
+
+    static void signin(ArrayList<User> use) {
+        System.out.println("Enter User name: ");
+        String signuser = sca.nextLine();
+        boolean isUser = false;
+        int i;
+        for(i = 0; i < ind; i++) {
+            if(signuser == use.get(i).getUserName()) {
+                isUser = true;
+                break;
+            }
+        }
+
+        String signpass;
+        if(isUser) {
+            System.out.println("Enter Password: ");
+            signpass = sca.nextLine();
+            
+            if(signpass == use.get(i).getPass()) {
+                System.out.println("Login Succesfull");
+                printFunctions();
+            }else {
+                System.out.println("Wrong Password");
+                System.out.println("Try Again");
+                signin(use);
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome. Please Sign Up to continue!");       
-        System.out.println("Enter User Name: ");
-        one = new User(sca.nextLine());
-        System.out.println("Create Password: ");
-        one.makeUser();                             // Calling makeUser method.
-        printFunctions();
+        
+        System.out.println("Welcome. Press 0 to Sign Up or 1 to Sign In!");
+        int signchoice=sca.nextInt();
+        if(signchoice == 0) signup(users);
+        else if(signchoice==1) {
+            System.out.println("enter username");
+            String username=new String();    
+        }
         
     }
 
-    //Savings obj = new Savings();
-    //obj.balance;
-
     static void printFunctions()
     { 
-        System.out.println("Welcome" + one.getUserName());
+        System.out.println("Welcome");
         System.out.println("The following functionalities are available for your use:");
         System.out.println("1. Accounts and balances");
         System.out.println("2. Tracking your Transactions");
@@ -146,6 +157,5 @@ public class Main {
         System.out.println("4. Debt Management");
         System.out.println("Enter the appropriate Digit to proceed further");
         int ch= sca.nextInt();
-        one.makeChoice1(ch);
     }
 }
